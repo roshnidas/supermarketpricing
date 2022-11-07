133431d1ea8d6cf0a917fa0d16d0b4ae49188eeb
Roshni Das <roshnidas8@gmail.com>
2022-11-07 17:47:21 +0530

    Initialize Springboot project

---
+++ src/main/java/com/policyexpert/supermarketpricing/SupermarketpricingApplication.java
@@ -0,0 +1,13 @@
+package com.policyexpert.supermarketpricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketpricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketpricingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/policyexpert/supermarketpricing/SupermarketpricingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policyexpert.supermarketpricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketpricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
